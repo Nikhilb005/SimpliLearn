11a9bd958485c0fc2d7a88b0bf06da0ce33560f8
Nikhilb005 <96907666+Nikhilb005@users.noreply.github.com>
2022-02-05 11:16:52 +0530

    Add files via upload

---
+++ Phase 2/Practice Project/ValidationOfUser/src/com/validation/Dashboard.java
@@ -0,0 +1,16 @@
+package com.validation;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Dashboard extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		 request.getRequestDispatcher("/link.html").include(request, response);
+	}
+
+}
